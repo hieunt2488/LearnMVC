d32ea57169c45ae836f9e03fdbbd83159451cc64
hieu.nguyentrung <hieu.nguyentrung@imipgroup.com>
2020-12-21 17:15:28 +0700

    update

---
+++ Example/DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,20 @@
+﻿//var button = document.getElementById("buyButton");
+//button.addEventListener("click", function () {
+//    console.log("Buying Item");
+//});
+
+//var productInfo = document.getElementsByClassName("product-props");
+//var listItem = productInfo.item[0].children;
+
+var theForm = $("#theForm");
+theForm.hide();
+
+var button = $("#buyButton");
+button.on("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
+});
