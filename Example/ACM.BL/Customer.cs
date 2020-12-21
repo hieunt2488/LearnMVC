33c440655a04705707fbe8592b603673189b331e
hieu.nguyentrung <hieu.nguyentrung@imipgroup.com>
2020-12-21 15:37:54 +0700

    add solution

---
+++ Example/ACM.BL/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        
+    }
+}
---
+++ Example/ExampleHTML2/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿alert("Hello cruel world!");
+console.log("Log this to the console!");
+
+function myDisplayer(some) {
+    document.getElementById("heading").innerHTML = some;
+}
+
+async function myFunction() {
+    return "Hello";
+}
+
+myFunction().then(
+    function (value) { myDisplayer(value); },
+    function (error) { myDisplayer(error); }
+);
